50a5b838d6d82cbb2d414b9008c20b8b8dc1a8f3
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-02-06 20:13:07 +0800

    create a reactnative app

---
+++ Project1/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaProvider }
+from 'react-native-safe-area-context';
+import FirstScreen from './src/screens/FirstScreen';
+
+const App = () => {
+    return (
+        <SafeAreaProvider>
+
+            <FirstScreen/>
+
+        </SafeAreaProvider>
+    )
+}
+
+export default App;
---
+++ Project1/src/screens/FirstScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FirstScreen = () => {
+    return (
+        <View>
+            <Text>
+                Hello World!
+            </Text>
+        </View>
+    )
+}
+
+export default FirstScreen;
