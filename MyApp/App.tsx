1ad38a738e076ea80f8cc439ad44e1b8278d10e2
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-02-13 21:18:20 +0800

    second screen

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import FirstScreen from './src/screens/FirstScreen';
+import SecondScreen from './src/screens/SecondScreen';
 
 const App = () => {
     return (
@@ -8,7 +9,8 @@ const App = () => {
              
              // This is how to use the components that have been made
              // in the FirstScreen.js File and use it in the App.js File
-             <FirstScreen/>
+             {/* <FirstScreen/> */}
+             <SecondScreen/>
 
         </SafeAreaProvider> 
     )
---
+++ MyApp/src/screens/SecondScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SecondScreen = () => {
+    return (
+        <View>
+            <View>
+                <Text>
+                    Mobile Legends
+                </Text>
+            </View>
+            <View>
+                <Text>
+                    PUBG
+                </Text>
+            </View>
+        </View>
+    )
+};
+
+export default SecondScreen;
