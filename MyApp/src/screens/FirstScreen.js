407d80bf91e2ae518b849539cf8cf119409e29e4
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-02-13 20:58:57 +0800

    create firstscreen

---
+++ MyApp/src/screens/FirstScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const FirstScreen = () => {
+    return (        
+        <View>
+            <Text>
+                // This is an example of an Element written in a Javascript Function
+                Hello World!
+            </Text>
+
+            <Button title="This is button"/>
+        </View>
+    )
+};
+
+export default FirstScreen;
