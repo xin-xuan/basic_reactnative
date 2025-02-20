b2b35046b3257ca512c6e49337290f7f64a865c3
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-02-20 21:14:21 +0800

    create third screen

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import FirstScreen from './src/screens/FirstScreen';
 import SecondScreen from './src/screens/SecondScreen';
+import ThirdScreen from './src/screens/ThirdScreen';
 
 const App = () => {
     return (
         <SafeAreaProvider>
              
              {/* <FirstScreen/> */}
-             <SecondScreen/>
+             {/* <SecondScreen/> */}
+             <ThirdScreen/>
 
         </SafeAreaProvider> 
     )
