463985622c151f7077ef873516e56f5edf1a23ef
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-02-20 20:29:09 +0800

    design button

--- MyApp/src/screens/SecondScreen.js
+++ MyApp/src/screens/SecondScreen.js
@@ -5,14 +5,27 @@ const SecondScreen = () => {
     return (
         <View style={{
             flex: 1,
-            backgroundColor: 'lavender'
+            backgroundColor: 'lavender',
+            justifyContent: 'center',
+            alignItems: 'center'
         }}>
-            <View style={{backgroundColor: 'skyblue'}}>
+            <View style={{
+                backgroundColor: 'skyblue',
+                padding: 10,
+                marginBottom: 5,
+                borderStyle: 'dashed'
+            }}>
                 <Text>
                     Mobile Legends
                 </Text>
             </View>
-            <View style = {{backgroundColor: 'cornflowerblue'}}>
+            <View style = {{
+                backgroundColor: 'cornflowerblue',
+                padding: 10,
+                marginTop: 5,
+                borderStyle: 'dotted'
+
+            }}>
                 <Text>
                     PUBG
                 </Text>
