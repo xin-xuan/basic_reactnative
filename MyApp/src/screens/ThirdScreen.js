fced2d2267fa0bfa6a7d9512405b3446193a2d14
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-02-20 21:29:50 +0800

    flex

--- MyApp/src/screens/ThirdScreen.js
+++ MyApp/src/screens/ThirdScreen.js
@@ -3,11 +3,41 @@ import { View, Text } from 'react-native';
 
 const ThirdScreen = () => {
     return (
-        <View>
-            <Text>
-                Pikachu
-            </Text>
+        <View style={{
+            flex: 1,
+            backgroundColor: 'mistyrose',
+            flexDirection: 'column'
+        }}>
+
+            <View style={{
+                backgroundColor: 'red',
+                flex: 1
+            }}>
+                <Text style={{ color: 'white' }}>
+                    Pikachu
+                </Text>
+            </View>
+
+            <View style={{
+                backgroundColor: 'orange',
+                flex: 2
+            }}>
+                <Text>
+                    Flareon
+                </Text>
+            </View>
+
+            <View style={{
+                backgroundColor: 'yellow',
+                flex: 3
+            }}>
+                <Text>
+                    Rapidesh
+                </Text>
+            </View>
+
         </View>
+
     )
 };
 
