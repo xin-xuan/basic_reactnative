f6057b713da0cf909097d5dfc4931bf658015076
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-03-06 21:01:11 +0800

    add phone number field

--- MyApp/src/screens/FourthScreen.js
+++ MyApp/src/screens/FourthScreen.js
@@ -30,11 +30,24 @@ const FourthScreen = () => {
             <TextInput
                 style={{
                     padding: 10, borderWidth: 1,
-                    height: 40, fontSize: 16
+                    height: 140, fontSize: 16
                 }}
                 placeholder="About the place"
                 multiline={true}
                 keyboardType="default"
+            />
+
+            <Text style={{ marginTop: 12, marginBottom: 12 }}>
+                Phone number
+            </Text>
+            <TextInput
+                style={{
+                    padding: 10, borderWidth: 1,
+                    height: 40, fontSize: 16
+                }}
+                placeholder="Phone number"
+                multiline={true}
+                keyboardType="numeric"
             />
         </View>
     )
