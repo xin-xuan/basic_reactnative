3694598db5e0ac61009fb6791644149ab691b40c
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-03-27 21:26:20 +0800

    create new screen

--- GuessTheCountry/App.tsx
+++ GuessTheCountry/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './src/screens/HomeScreen';
+import GuessTheCountryScreen from './src/screens/GuessTheCountryScreen';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <GuessTheCountryScreen/>
     </SafeAreaProvider>
   )
 };
