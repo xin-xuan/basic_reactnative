1d1096cad162c46c485f68b1a03afe7ed2020f99
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-04-10 20:45:46 +0800

    create winscreen

--- GuessTheCountry/App.tsx
+++ GuessTheCountry/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './src/screens/HomeScreen';
 import GuessTheCountryScreen from './src/screens/GuessTheCountryScreen';
+import WinScreen from './src/screens/WinScreen';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       {/* <HomeScreen /> */}
-      <GuessTheCountryScreen/>
+      {/* <GuessTheCountryScreen/> */}
+      <WinScreen/>
     </SafeAreaProvider>
   )
 };
