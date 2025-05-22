1f9735ccbd84f85fdc2224f2dbf16fc739fe8524
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-05-22 20:35:16 +0800

    MovieCollection

---
+++ dummy/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TryCodeScreen from './src/screens/TryCodeScreen';
+
+const App = () => {
+    return (
+        <SafeAreaProvider>
+             <TryCodeScreen/>
+        </SafeAreaProvider> 
+    )
+}
+
+export default App;
