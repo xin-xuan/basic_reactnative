0f449b71de8abaafdb507e727627754ea7aa769b
xin-xuan <158292934+xin-xuan@users.noreply.github.com>
2025-08-14 20:31:40 +0800

    rename

--- MovieCollection/android/app/src/main/java/com/project2/MainActivity.kt
+++ MovieCollection/android/app/src/main/java/com/project2/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Project2"
+  override fun getMainComponentName(): String = "MovieCollection"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
